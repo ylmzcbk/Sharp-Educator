98964c16dc563649a7be4ee17f3655f07705f0c8
Yılmaz ÇABUK <yilmazcabuk@marun.edu.tr>
2021-02-22 13:39:40 +0300

    Add project files.

---
+++ Uygulama.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace SharpEducator
+{
+    partial class Uygulama
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Uygulama));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Hakkinda = new System.Windows.Forms.Button();
+            this.Ayarlar = new System.Windows.Forms.Button();
+            this.Iletisim = new System.Windows.Forms.Button();
+            this.PLabel = new System.Windows.Forms.Panel();
+            this.Bolumler = new System.Windows.Forms.Button();
+            this.AnaSayfa = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(0)))), ((int)(((byte)(58)))));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 15);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.Hakkinda);
+            this.panel2.Controls.Add(this.Ayarlar);
+            this.panel2.Controls.Add(this.Iletisim);
+            this.panel2.Controls.Add(this.PLabel);
+            this.panel2.Controls.Add(this.Bolumler);
+            this.panel2.Controls.Add(this.AnaSayfa);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 450);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bariol Bold", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Orange;
+            this.label1.Location = new System.Drawing.Point(18, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 26);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "SHARP EDUCATOR";
+            // 
+            // Hakkinda
+            // 
+            this.Hakkinda.FlatAppearance.BorderSize = 0;
+            this.Hakkinda.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Hakkinda.Font = new System.Drawing.Font("MIUI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Hakkinda.ForeColor = System.Drawing.Color.White;
+            this.Hakkinda.Image = global::SharpEducator.Properties.Resources.info;
+            this.Hakkinda.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Hakkinda.Location = new System.Drawing.Point(22, 382);
+            this.Hakkinda.Name = "Hakkinda";
+            this.Hakkinda.Size = new System.Drawing.Size(178, 68);
+            this.Hakkinda.TabIndex = 7;
+            this.Hakkinda.Text = "Hakkında";
+            this.Hakkinda.UseVisualStyleBackColor = true;
+            this.Hakkinda.Click += new System.EventHandler(this.Hakkinda_Click);
+            // 
+            // Ayarlar
+            // 
+            this.Ayarlar.FlatAppearance.BorderSize = 0;
+            this.Ayarlar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Ayarlar.Font = new System.Drawing.Font("MIUI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ayarlar.ForeColor = System.Drawing.Color.White;
+            this.Ayarlar.Image = global::SharpEducator.Properties.Resources.cogwheel;
+            this.Ayarlar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Ayarlar.Location = new System.Drawing.Point(23, 234);
+            this.Ayarlar.Name = "Ayarlar";
+            this.Ayarlar.Size = new System.Drawing.Size(178, 68);
+            this.Ayarlar.TabIndex = 6;
+            this.Ayarlar.Text = "Ayarlar";
+            this.Ayarlar.UseVisualStyleBackColor = true;
+            this.Ayarlar.Click += new System.EventHandler(this.Ayarlar_Click);
+            // 
+            // Iletisim
+            // 
+            this.Iletisim.FlatAppearance.BorderSize = 0;
+            this.Iletisim.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Iletisim.Font = new System.Drawing.Font("MIUI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Iletisim.ForeColor = System.Drawing.Color.White;
+            this.Iletisim.Image = global::SharpEducator.Properties.Resources.message;
+            this.Iletisim.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Iletisim.Location = new System.Drawing.Point(22, 308);
+            this.Iletisim.Name = "Iletisim";
+            this.Iletisim.Size = new System.Drawing.Size(178, 68);
+            this.Iletisim.TabIndex = 5;
+            this.Iletisim.Text = "İletişim";
+            this.Iletisim.UseVisualStyleBackColor = true;
+            this.Iletisim.Click += new System.EventHandler(this.Iletisim_Click);
+            // 
+            // PLabel
+            // 
+            this.PLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(0)))), ((int)(((byte)(58)))));
+            this.PLabel.Location = new System.Drawing.Point(0, 86);
+            this.PLabel.Name = "PLabel";
+            this.PLabel.Size = new System.Drawing.Size(16, 68);
+            this.PLabel.TabIndex = 3;
+            // 
+            // Bolumler
+            // 
+            this.Bolumler.FlatAppearance.BorderSize = 0;
+            this.Bolumler.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Bolumler.Font = new System.Drawing.Font("MIUI", 15F);
+            this.Bolumler.ForeColor = System.Drawing.Color.White;
+            this.Bolumler.Image = global::SharpEducator.Properties.Resources.file;
+            this.Bolumler.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Bolumler.Location = new System.Drawing.Point(22, 160);
+            this.Bolumler.Name = "Bolumler";
+            this.Bolumler.Size = new System.Drawing.Size(178, 68);
+            this.Bolumler.TabIndex = 4;
+            this.Bolumler.Text = "Bölümler";
+            this.Bolumler.UseVisualStyleBackColor = true;
+            this.Bolumler.Click += new System.EventHandler(this.Bolumler_Click);
+            // 
+            // AnaSayfa
+            // 
+            this.AnaSayfa.FlatAppearance.BorderSize = 0;
+            this.AnaSayfa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AnaSayfa.Font = new System.Drawing.Font("MIUI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AnaSayfa.ForeColor = System.Drawing.Color.White;
+            this.AnaSayfa.Image = global::SharpEducator.Properties.Resources.homepage;
+            this.AnaSayfa.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AnaSayfa.Location = new System.Drawing.Point(22, 86);
+            this.AnaSayfa.Name = "AnaSayfa";
+            this.AnaSayfa.Size = new System.Drawing.Size(178, 68);
+            this.AnaSayfa.TabIndex = 3;
+            this.AnaSayfa.Text = "Ana Sayfa";
+            this.AnaSayfa.UseVisualStyleBackColor = true;
+            this.AnaSayfa.Click += new System.EventHandler(this.AnaSayfa_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(0)))), ((int)(((byte)(58)))));
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Location = new System.Drawing.Point(206, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(100, 112);
+            this.panel3.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(10, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 83);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Uygulama
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Uygulama";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sharp Educator";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button AnaSayfa;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button Bolumler;
+        private System.Windows.Forms.Panel PLabel;
+        private System.Windows.Forms.Button Hakkinda;
+        private System.Windows.Forms.Button Ayarlar;
+        private System.Windows.Forms.Button Iletisim;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Uygulama.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SharpEducator
+{
+    public partial class Uygulama : Form
+    {
+        public Uygulama()
+        {
+            InitializeComponent();
+        }
+
+        private void AnaSayfa_Click(object sender, EventArgs e)
+        {
+            PLabel.Top = AnaSayfa.Top;
+        }
+
+        private void Bolumler_Click(object sender, EventArgs e)
+        {
+            PLabel.Top = Bolumler.Top;
+        }
+
+        private void Ayarlar_Click(object sender, EventArgs e)
+        {
+            PLabel.Top = Ayarlar.Top;
+        }
+
+        private void Iletisim_Click(object sender, EventArgs e)
+        {
+            PLabel.Top = Iletisim.Top;
+        }
+
+        private void Hakkinda_Click(object sender, EventArgs e)
+        {
+            PLabel.Top = Hakkinda.Top;
+        }
+    }
+}
